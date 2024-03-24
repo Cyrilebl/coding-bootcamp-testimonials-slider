21b9a088b765f1ace60f4a9257e8198c1de00697
Cyril Leblanc <cyril.leblanc@outlook.com>
2024-03-24 13:26:48 -0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+const testimonials = document.querySelectorAll("section");
+const nbSlide = testimonials.length;
+let count = 0;
+
+function nextSlide() {
+  testimonials[count].classList.remove("active");
+  count = (count + 1) % nbSlide;
+  testimonials[count].classList.add("active");
+}
+
+function previewSlide() {
+  testimonials[count].classList.remove("active");
+  count = (count - 1 + nbSlide) % nbSlide;
+  testimonials[count].classList.add("active");
+}
+
+function keyPress(e) {
+  if (e.keyCode === 39) {
+    nextSlide();
+  } else if (e.keyCode === 37) {
+    previewSlide();
+  }
+}
+
+document.querySelector(".next").addEventListener("click", nextSlide);
+
+document.querySelector(".prev").addEventListener("click", previewSlide);
+
+document.addEventListener("keydown", keyPress);
